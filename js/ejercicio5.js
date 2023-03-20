9d8361e3c03e9204cc781e138ec9542071fa9023
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2023-03-20 00:52:16 -0300

    Ejercicio 5 Resuelto

---
+++ js/ejercicio5.js
@@ -0,0 +1,28 @@
+// 5- Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. 
+// A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, 
+// sólo por minúsculas o por una mezcla de ambas.
+
+const cadena = prompt('Ingrese una cadena de texto: ');
+let banderaMayus = false;
+let banderaMinus = false;
+
+function cadenaMayusculaMinuscula(cadena){
+    for(let indice=0;indice<cadena.length;indice++){
+        if(cadena.charAt(indice) === cadena.charAt(indice).toLowerCase() && cadena.charAt(indice) != ' ' ){
+            banderaMinus=true;
+        }
+        if(cadena.charAt(indice) === cadena.charAt(indice).toUpperCase() && cadena.charAt(indice) != ' '){
+            banderaMayus=true;
+        }
+    }
+    if(banderaMayus && banderaMinus){
+        return 'Contiene Mayúsculas y Minúsculas';
+    }
+    if(banderaMayus){
+        return 'Contiene solo Mayúsculas';
+    }
+    if(banderaMinus){
+        return 'Contiene solo Minúsculas';
+    }
+}
+document.write(cadenaMayusculaMinuscula(cadena));
